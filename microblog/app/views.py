06d827a831073a5a7bb3bf46c9236debf21ef0f3
ailtoncsf <ailtoncsf@yahoo.com.br>
2016-04-15 15:26:37 -0300

    Ajustes no exercicio.

--- microblog/app/views.py
+++ microblog/app/views.py
@@ -3,7 +3,25 @@ from app import app
 from .forms import LoginForm
 
 
-# index view function suppressed for brevity
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'nickname': 'Miguel'}
+    posts = [
+        {
+            'author': {'nickname': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'nickname': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html',
+                           title='Home',
+                           user=user,
+                           posts=posts)
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
