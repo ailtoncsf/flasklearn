ac33a3afe91baadbc79f2d9dfb1122e12910508e
ailtoncsf <ailtoncsf@yahoo.com.br>
2016-04-15 15:20:52 -0300

    Adicionando SQLAlchemy.

--- microblog/config.py
+++ microblog/config.py
@@ -1,3 +1,12 @@
+import os
+
+# Flask config.py
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+#SQLALCHEMY_DATABASE_URI = "postgresql://postgres:123456@localhost/flask"
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
 
