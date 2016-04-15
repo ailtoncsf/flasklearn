b5df1bb03f84464f7bf6c939db32f1dcfab4335f
ailtoncsf <ailtoncsf@yahoo.com.br>
2016-04-15 18:04:09 -0300

    Adicionando arquivo python conetando com postgres

---
+++ flask_postgres/test_psycopg2.py
@@ -0,0 +1,27 @@
+import psycopg2
+import sys
+
+db_con = None
+try:
+
+    # Create a database session
+    db_con = psycopg2.connect(database='webgis', user='postgres', password='123456')
+
+    # Create a client cursor to execute commands
+    cursor = db_con.cursor()
+    cursor.execute("CREATE TABLE customers (id SERIAL PRIMARY KEY, name VARCHAR age INTEGER);")
+
+    # The variables placeholder must always be a %s, psycop2 will automatically convert the values to SQL literal
+    cursor.execute("INSERT INTO customers ( name, AGE) VALUES (%s, %s)", ("leo", 26))
+    db_con.commit()
+    cursor.execute("SELECT * FROM customers")
+    print(cursor.fetchone())
+
+except Exception as e:
+
+    print ('Error %s' % e)
+    sys.exit(1)
+
+finally:
+    if db_con:
+        db_con.close()
